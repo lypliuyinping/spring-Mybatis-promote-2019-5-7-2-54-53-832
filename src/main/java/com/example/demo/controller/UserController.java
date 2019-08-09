b775e66d61c6e37df00f70f768185bfd74c412e6
liuyinping <1689372042@qq.com>
2019-08-09 17:43:31 +0800

    更新

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +16,27 @@ public class UserController {
 
     @GetMapping("/user")
     public List<User> findAll() {
-        return null;
+        PageHelper.startPage(2,2);
+        return userMapper.findAll();
     }
 
     @GetMapping("/user/{userId}")
-    public User findById(@PathVariable long userId) {
-        return null;
+    public User findById(@PathVariable Integer userId) {
+        return userMapper.selectByPrimaryKey(userId);
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.insert(user);
     }
 
     @PutMapping("/user/{userId}")
     public void updateUser(@RequestBody User user,@PathVariable int userId) {
+        userMapper.updateByPrimaryKey(user,userId);
     }
 
     @DeleteMapping("/user/{userId}")
-    public void deleteById(@PathVariable long userId) {
+    public void deleteById(@PathVariable Integer userId) {
+        userMapper.deleteByPrimaryKey(userId);
     }
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,18 +1,17 @@
 package com.example.demo.entity;
 
 public class User {
-    private long id;
+    private Integer id;
     private String name;
 
 
-    public long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
-
 
     public String getName() {
         return name;
@@ -21,6 +20,5 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-
 }
 
